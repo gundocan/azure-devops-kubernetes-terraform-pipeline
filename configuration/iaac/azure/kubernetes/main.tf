890bf0615fb133c46bcf9c7badc07f573ed4c57a
gundocan <73230961+gundocan@users.noreply.github.com>
2022-07-16 23:16:33 +0200

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "standard_ec2as_v5"
+    vm_size         = "Standard_ec_v5"
   }
 
   service_principal {
